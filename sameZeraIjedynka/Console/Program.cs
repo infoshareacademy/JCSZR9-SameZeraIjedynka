f047a96393c4d4f7beeac647f8154d1dcf8a77d3
PJO <mojinfoshare@gmail.com>
2023-03-02 08:33:26 +0000

    Menu functionality - first draft

--- sameZeraIjedynka/Console/Program.cs
+++ sameZeraIjedynka/Console/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu MainMenu = new Menu();
+            MainMenu.PrintWelcomeScreen();
         }
     }
 }
---
+++ sameZeraIjedynka/Console/SubMenu/Ulubione.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console.SubMenu
+{
+    public class Ulubione
+    {
+        private char _selection { get; set; }
+
+        public Ulubione(char Selection)
+        {
+            _selection = Selection;
+
+            switch (_selection)
+            {
+                case 'a':
+                    DodanieLubSkasowanie();
+                    break;
+                case 'b':
+                    WyswietlenieUlubionych();
+                    break;
+                case 'c':
+                    PokazNajblizsze();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void DodanieLubSkasowanie()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Dodanie lub skasowanie");
+            System.Console.Read();
+        }
+        public void WyswietlenieUlubionych()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Wyswietlenie ulubionych");
+            System.Console.Read();
+        }
+        public void PokazNajblizsze()
+        {
+            System.Console.Clear();
+            System.Console.WriteLine("Pokaz najblizsze");
+            System.Console.Read();
+        }
+
+    }
+}
