87cf42638b21fc5e0d8e563897b0821ad0a364dc
PJO <mojinfoshare@gmail.com>
2023-03-19 19:43:31 +0000

    fix

---
+++ sameZeraIjedynka/ConsoleApp/SubMenu/Events.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessCase.Controllers;
+using BusinessCase.Model;
+using static System.IO.Directory;
+
+namespace ConsoleApp
+{
+    
+    public class Events
+    {
+        private char _selection { get; set; }
+
+        public Events(char Selection)
+        {
+            _selection = Selection;
+
+            switch (_selection)
+            {
+                case 'a':
+                    EventList();
+                    break;
+                case 'b':
+                    DateFilter();
+                    break;
+                case 'c':
+                    EventFinder();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void DisplayEvents(List<Event> events)
+        {
+            if (events.Count > 0)
+                foreach (var item in events)
+                {
+                    if (item.IsFavourite)
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.WriteLine($"#{item.Id} {item.Name} \n " +
+                        $"\t{item.Price}$ {item.Date} {item.Place}\n" +
+                        $"\tCapacity: {item.Capacity}\n" +
+                        $"\tOrganizer: {item.Organizer}\n" +
+                        $"\tTarget: {item.Target}");
+
+                    if (item.IsFavourite)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+            else
+                Console.WriteLine("No events found");
+        }
+
+        private void EventList()
+        {
+            Console.Clear();
+            Console.WriteLine("--------- Events list ---------\n");
+
+            var events = EventController.GetEvents();
+            DisplayEvents(events);
+
+            Console.Read();
+        }
+
+        private void DateFilter()
+        {
+            Console.Clear();
+            Console.WriteLine("--------- Filter by date ---------\n");
+
+            CultureInfo.CurrentUICulture = new CultureInfo("en-US", false);
+
+            Console.WriteLine("Input start date (in MM/DD/YYYY format):");
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), 
+                "MM/dd/yyyy", 
+                CultureInfo.CurrentUICulture);
+
+            Console.WriteLine("Input end date (in MM/DD/YYYY format):");
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), 
+                "MM/dd/yyyy", 
+                CultureInfo.CurrentUICulture);
+
+            var events = EventController.GetEvents().Where(e => 
+                (e.Date >= startDate) && 
+                (e.Date <= endDate)).ToList();
+            Console.Clear();
+            Console.WriteLine("--------- Filter by date ---------\n");
+            DisplayEvents(events);
+
+            Console.Read();
+        }
+
+        private void EventFinder()
+        {
+            Console.Clear();
+            Console.WriteLine("--------- Event search ---------\n");
+
+            Console.WriteLine("Input pattern:");
+            var searchPattern = Console.ReadLine();
+
+            var events = EventController.GetEvents().Where(e => 
+                (e.Name.Contains(searchPattern)) ||
+                (e.Place.Contains(searchPattern)) ||
+                (e.Organizer.Contains(searchPattern))).ToList();
+            Console.Clear();
+            Console.WriteLine("--------- Event search ---------\n");
+            DisplayEvents(events);
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ sameZeraIjedynka/ConsoleApp/SubMenu/Favorites.cs
@@ -0,0 +1,76 @@
+﻿using BusinessCase.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Favorites
+    {
+        private char _selection { get; set; }
+
+
+
+        public Favorites(char Selection)
+        {
+            _selection = Selection;
+           
+            {
+                switch (_selection)
+                {
+                    case 'a':
+                        AddOrRemove();
+                        break;
+                    case 'b':
+                        FavoriteDisplay();
+                        break;
+                    case 'c':
+                        ShowNextEvent();
+                        break;
+                    default:
+                        break;
+                }
+            }
+           
+        }
+        public void AddOrRemove()
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Add or remove");
+            Console.WriteLine("Enter event details: title, date, organizer, type.");
+            var id = int.Parse(Console.ReadLine());
+            var name = Console.ReadLine();
+            DateTime.TryParse(Console.ReadLine(), out var date);
+            var organizer = Console.ReadLine();
+            var place = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out int price);
+            var capacity = int.Parse(Console.ReadLine());
+            Enum.TryParse(Console.ReadLine(), out TargetEnum target);
+            bool.TryParse(Console.ReadLine(), out bool isFavourite);
+ 
+            var newEvent = new vFavorite(id, name, date, organizer, place, price, capacity, target, isFavourite);
+
+            //var favoriteEvent = new vFavorite();
+            //favoriteEvent.AddFavorite(newEvent);
+        }
+        public void FavoriteDisplay()
+        {
+            //var favoriteEvent = new vFavorite();
+            Console.Clear();
+            Console.WriteLine("Display the event");
+            //favoriteEvent.DisplayAllFavoriteEvents();
+        }
+        public void ShowNextEvent()
+        {
+            Console.Clear();
+            Console.WriteLine("Display next event");
+            Console.Read();
+        }
+
+    }
+}
