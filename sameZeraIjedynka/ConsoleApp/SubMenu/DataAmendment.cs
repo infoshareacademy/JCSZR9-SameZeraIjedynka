8b881f17bbbebd140ad932e40e7d28235e840a4b
PJO <mojinfoshare@gmail.com>
2023-03-24 18:50:37 +0000

    AmendmentInterface

--- sameZeraIjedynka/ConsoleApp/SubMenu/DataAmendment.cs
+++ sameZeraIjedynka/ConsoleApp/SubMenu/DataAmendment.cs
@@ -16,31 +16,34 @@ namespace ConsoleApp.SubMenu
      
     public class DataAmendment
     {
-        
-
-
         public void CollectData()
         {
             var events = EventManager.GetEvents();
             DisplayEvents(events);
-          
+            AmendmentInterface(events);
         }
 
         public static void DisplayEvents(List<EventModel> events)
         {
             if (events.Count > 0)
                 foreach (var item in events)
-                {
-                    if (item.IsFavourite)
-                        Console.ForegroundColor = ConsoleColor.Green;
-
+                {        
                     Console.WriteLine(item);
-
-                    if (item.IsFavourite)
-                        Console.ForegroundColor = ConsoleColor.Gray;
                 }
             else
                 Console.WriteLine("No events found");
+        }
+
+        public void AmendmentInterface(List<EventModel> events)
+        {
+            Console.WriteLine("\n Write name of the event you would like to edit:");
+            string ?nameToBeAmend = Console.ReadLine();
+            Console.WriteLine(events.FirstOrDefault(a=>a.Name.Contains(nameToBeAmend)));
+            Console.WriteLine("\n Which parametre you would like to change: name, capacity, organiser, or terget ?");
+            string ?parameter = Console.ReadLine();
+            Console.WriteLine("\n What is new value of this parameter ?"); 
+            string ?newValue = Console.ReadLine();
+            Console.ReadLine();
         }
 
 
