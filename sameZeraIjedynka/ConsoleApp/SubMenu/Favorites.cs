12d5e2d25251aa3557138896dba29f6dd33fcd9d
PaulinaBednarek <paulina.bednarek.inf@gmail.com>
2023-03-24 20:26:55 +0100

    dodane zmiany w sekcji ulubione

--- sameZeraIjedynka/ConsoleApp/SubMenu/Favorites.cs
+++ sameZeraIjedynka/ConsoleApp/SubMenu/Favorites.cs
@@ -1,4 +1,5 @@
-﻿using BusinessCase.Model;
+﻿using BusinessCase.Controllers;
+using BusinessCase.Model;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,23 +14,72 @@ namespace ConsoleApp
     {
         private char Selection { get; set; }
 
-
-
         public Favorites(char Selection)
         {
             this.Selection = Selection;
-           
+
             {
                 switch (this.Selection)
                 {
                     case 'a':
-                        AddOrRemove();
+                        Console.WriteLine("Enter the name of the event you want to add or remove to your favourites:");
+                        string name = Console.ReadLine();
+
+                        var selectedEvent = EventManager.GetEvents().FirstOrDefault(e => e.Name.Contains(name));
+                        if (selectedEvent != null)
+                        {
+                            Console.WriteLine($"If you want to add an event '{selectedEvent.Name}' to your favourites press Y.");
+                            Console.WriteLine($"If you want to remove an event '{selectedEvent.Name}' from your favourites press N.");
+                            ConsoleKeyInfo key = Console.ReadKey();
+                            if (key.KeyChar == 'Y')
+                            {
+                                selectedEvent.IsFavourite = true;
+                                Console.WriteLine($"Event '{selectedEvent.Name}' added to your favourites.");
+                            }
+                            else if (key.KeyChar == 'N')
+                            {
+                                selectedEvent.IsFavourite = false;
+                                Console.WriteLine($"Event '{selectedEvent.Name}' deleted from your favourites.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Unavailable move.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Can't find event '{name}'.");
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\n Press any key to return.");
+                        Console.ReadKey(true);
+                        Console.Clear();
+
                         break;
                     case 'b':
-                        FavoriteDisplay();
+                        var fav = EventManager.GetEvents().Where(f => f.IsFavourite == true).ToList();
+
+                        Events.DisplayEvents(fav);
+
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\n Press any key to return.");
+                        Console.ReadKey(true);
+                        Console.Clear();
                         break;
                     case 'c':
-                        ShowNextEvent();
+                        Console.WriteLine("Enter the search phrase");
+                        var searchPharse = Console.ReadLine() ?? string.Empty;
+
+                        var search = EventManager.GetEvents(searchPharse);
+                        Console.WriteLine();
+                        Events.DisplayEvents(search);
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\n Press any key to return.");
+                        Console.ReadKey(true);
+                        Console.Clear();
                         break;
                     case '0':
                         Console.Clear();
@@ -40,55 +90,7 @@ namespace ConsoleApp
                         break;
                 }
             }
-           
-        }
-        public void AddOrRemove()
-        {
-            
-            Console.Clear();
-            Console.WriteLine("Add or remove");
-            Console.WriteLine("Enter event details: title, date, organizer, type.");
-            var id = int.Parse(Console.ReadLine());
-            var name = Console.ReadLine();
-            DateTime.TryParse(Console.ReadLine(), out var date);
-            var organizer = Console.ReadLine();
-            var place = Console.ReadLine();
-            int.TryParse(Console.ReadLine(), out int price);
-            var capacity = int.Parse(Console.ReadLine());
-            Enum.TryParse(Console.ReadLine(), out Target target);
-            bool.TryParse(Console.ReadLine(), out bool isFavourite);
- 
-            var newEvent = new vFavorite(id, name, date, organizer, place, price, capacity, target, isFavourite);
 
-            //var favoriteEvent = new vFavorite();
-            //favoriteEvent.AddFavorite(newEvent);
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n Press any key to return.");
-            Console.ReadKey(true);
-            Console.Clear();
-        }
-        public void FavoriteDisplay()
-        {
-            //var favoriteEvent = new vFavorite();
-            Console.Clear();
-            Console.WriteLine("Display the event");
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n Press any key to return.");
-            Console.ReadKey(true);
-            Console.Clear();
-            //favoriteEvent.DisplayAllFavoriteEvents();
-        }
-        public void ShowNextEvent()
-        {
-            Console.Clear();
-            Console.WriteLine("Display next event");
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("\n Press any key to return.");
-            Console.ReadKey(true);
-            Console.Clear();
         }
 
     }
