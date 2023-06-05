cc22875b856a9fa5e59f7744a58cc327f4e828f1
PJO <mojinfoshare@gmail.com>
2023-06-05 06:36:58 +0100

    User Index and View is working

--- sameZeraIjedynka/SameZeraIJedynka/Controllers/UsersController.cs
+++ sameZeraIjedynka/SameZeraIJedynka/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SameZeraIJedynka.Models;
 using SameZeraIjedynka.Database.Entities;
 using SameZeraIjedynka.Database.Context;
+
 
 namespace SameZeraIJedynka.Controllers
 {
@@ -34,5 +36,34 @@ namespace SameZeraIJedynka.Controllers
             await mvcDbContext.SaveChangesAsync();
             return RedirectToAction("Add");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var users = await mvcDbContext.Users.ToListAsync();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult View(int id)
+        {
+            var user = mvcDbContext.Users.FirstOrDefault(x => x.UserId == id);
+      
+            if (user != null)
+            {
+                var viewModel = new UpdateUserViewModel()
+                {
+                    UserId = user.UserId,
+                    Name = user.Name,
+                    Password = user.Password
+                };
+                return  View(viewModel);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+
     }
 }
---
+++ sameZeraIjedynka/SameZeraIJedynka/Models/UpdateUserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SameZeraIJedynka.Models
+{
+    public class UpdateUserViewModel
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
