839d50d2a6da5ef3ca8bfb52416d4e0ce0364676
PaulinaBednarek <paulina.bednarek.inf@gmail.com>
2023-07-09 13:48:18 +0200

    dodanie interfejsu uzytkownika

--- sameZeraIjedynka/SameZeraIjedynka.BusinnessLogic/Services/IUserService.cs
+++ sameZeraIjedynka/SameZeraIjedynka.BusinnessLogic/Services/IUserService.cs
@@ -10,6 +10,10 @@ namespace SameZeraIjedynka.BusinnessLogic.Services
     public interface IUserService
     {
        Task Add(UserModel addUserRequest);
+       Task<List<UserModel>> GetAllUsers();
+       Task<UserModel> GetUserById(int id);
+       Task UpdateUser(UserModel model);
+       Task DeleteUser(UserModel model);
     }
 }
 
