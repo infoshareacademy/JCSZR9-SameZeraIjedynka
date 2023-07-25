4101002ad8036ff0e610d28a296e3f50310ffbde
kwidz <kwidzinskio@gmail.com>
2023-07-25 20:34:08 +0200

    disallowed price and capacity <0 in add event form

--- sameZeraIjedynka/SameZeraIjedynka.BusinnessLogic/Models/EventModel.cs
+++ sameZeraIjedynka/SameZeraIjedynka.BusinnessLogic/Models/EventModel.cs
@@ -10,14 +10,10 @@ namespace SameZeraIJedynka.BusinnessLogic.Models
     {
         public int EventId { get; set; }
         public string Name { get; set; }
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-ddTHH:mm}")]
         public DateTime Date { get; set; }
         public string Organizer { get; set; }
         public string Place { get; set; }
-        //TODO: not allow <0
         public int Price { get; set; }
-        //TODO: not allow <0
         public int Capacity { get; set; }
         public TargetEnum Target { get; set; }
         public string Description { get; set; }
